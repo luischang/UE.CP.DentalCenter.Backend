99f047fd2c8c50791e44fc65e7c92ec7d0151e5a
nayduramon <naydu.3005@gmail.com>
2022-10-04 08:11:05 -0500

    Se agregó el usuario controller

---
+++ UE.CP.DentalCenter.API/Controllers/UsuarioController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UE.CP.DentalCenter.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+    }
+}
