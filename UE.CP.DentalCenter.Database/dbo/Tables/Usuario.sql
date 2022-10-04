70db7a0c7a493e7423c88216c0a6d21cfe313c90
Luis Chang <luis.chang@outlook.com>
2022-10-04 01:22:43 -0500

    Add database project

---
+++ UE.CP.DentalCenter.Database/dbo/Tables/Usuario.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Usuario] (
+    [Id]              INT            NOT NULL,
+    [Paterno]         NVARCHAR (100) NULL,
+    [Materno]         NVARCHAR (100) NULL,
+    [Nombres]         NVARCHAR (100) NULL,
+    [NumeroDocumento] INT            NULL,
+    [Correo]          NVARCHAR (100) NULL,
+    [Direccion]       NVARCHAR (100) NULL,
+    [Teléfono]        NVARCHAR (100) NULL,
+    [TipoUsuario]     NVARCHAR (50)  NULL,
+    [Contrasena]      NVARCHAR (100) NULL,
+    [Estado] NCHAR(5) NULL, 
+    CONSTRAINT [PK_Usuario] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
