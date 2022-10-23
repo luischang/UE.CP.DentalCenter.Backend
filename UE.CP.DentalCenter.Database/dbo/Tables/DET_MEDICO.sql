128caeea1ecac2a0aa72253ea30035abc81b3160
Christopher207 <joseaaron207@gmail.com>
2022-10-23 07:33:24 -0500

    Hotfix 23.10.22

--- UE.CP.DentalCenter.Database/dbo/Tables/DET_MEDICO.sql
+++ UE.CP.DentalCenter.Database/dbo/Tables/DET_MEDICO.sql
@@ -3,5 +3,6 @@
     [idMedico] int Not null,
     [idEspecialidad]  int NOT NULL, 
     CONSTRAINT [PK_DET_MEDICO] PRIMARY KEY ([idDetMedico]), 
-    CONSTRAINT [FK_DET_MEDICO_ESPECIALIDAD] FOREIGN KEY ([idEspecialidad]) REFERENCES [ESPECIALIDAD]([idEspecialidad]),
+    CONSTRAINT [FK_DET_MEDICO_ESPECIALIDAD] FOREIGN KEY ([idEspecialidad]) REFERENCES [ESPECIALIDAD]([idEspecialidad]), 
+    CONSTRAINT [FK_DET_MEDICO_CAB_MEDICO] FOREIGN KEY ([idMedico]) REFERENCES [CAB_MEDICO]([idMedico]),
 );
